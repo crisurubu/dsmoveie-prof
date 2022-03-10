f4411e2566821b3887587f3e21e5f0011104ed33
crisurubu <cristianourubu@gmail.com>
2022-03-10 19:08:54 -0300

    Project create

---
+++ backend/src/main/java/com/crisurubu/dsmovieprof/DsmovieProfApplication.java
@@ -0,0 +1,13 @@
+package com.crisurubu.dsmovieprof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieProfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieProfApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crisurubu/dsmovieprof/DsmovieProfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisurubu.dsmovieprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
